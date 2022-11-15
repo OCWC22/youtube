d254bef6b06b48df887ae2554557056108b29002
William Chen <57119977+OCWC22@users.noreply.github.com>
2022-11-14 23:45:00 -0800

    merge sort

--- sort/merge_sort.py
+++ sort/merge_sort.py
@@ -1,16 +1,14 @@
-def merge_sort(list):
-    length = len(list)
 
-    if length == 1:
+def merge_sort(list):
+    if len(list) <= 1:
         return list
 
-    mid = length // 2
-
-    left = merge_sort(list[:mid])
-    right = merge_sort(list[mid:])
+    middle = len(list) // 2
+    left = merge_sort(list[:middle])
+    right = merge_sort(list[middle:])
 
     return merge(left, right)
-
+    
 
 def merge(left, right):
     output = []
